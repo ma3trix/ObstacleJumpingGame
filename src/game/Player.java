324339f13ad1ec8320210d9b408fe029049129a4
malikadebiyi <malikbiyi@gmail.com>
2024-10-23 15:56:15 -0400

    modified player/obstacle movement

--- src/game/Player.java
+++ src/game/Player.java
@@ -39,7 +39,7 @@ public class Player {
     public void jump() {
         if (!isJumping) {
             isJumping = true;
-            jumpSpeed = -22; // Set initial jump speed
+            jumpSpeed = -24; // Set initial jump speed
         }
     }
 
